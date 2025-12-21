4de26d70e2c884ecf98ef19e3c7f33ba52488bca
z003t6tb <akos.kiszely.ext@siemens-healthineers.com>
2025-12-21 21:57:25 +0100

    Logo

--- app/app.vue
+++ app/app.vue
@@ -11,7 +11,7 @@ useHead({
   }
 })
 
-const title = 'Nuxt Starter Template'
+const title = 'VersesKötetem'
 const description = 'A production-ready starter template powered by Nuxt UI. Build beautiful, accessible, and performant applications in minutes, not hours.'
 
 useSeoMeta({
